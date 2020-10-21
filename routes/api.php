732def372e55d51a09ade360717eaf3dbba6e926
Nayeli <nayeliesquivelluna@gmail.com>
2020-10-20 22:13:24 -0500

    creación y modificación de rutas

--- routes/api.php
+++ routes/api.php
@@ -21,15 +21,25 @@ Route::get('foo', function () {
     return 'Hello World';
 });
 
+/* Rutas Personas*/
 Route::get('personas/{id?}', [PersonasController::class, 'getPersonas'])->where("id", "[0-9]+");
 Route::post('personas', [PersonasController::class, 'createPersona']);
+Route::put('/updatepersona/{id}', [PersonasController::class, 'updatePersona']);
+Route::delete('/deletepersona/{id}', [PersonasController::class, 'deletePersona']);
 
+/* Rutas Productos*/
 Route::get('productos/{id?}', [ProductosController::class, 'getProductos'])->where("id", "[0-9]+");
 Route::post('productos', [ProductosController::class, 'createProducto']);
+Route::put('/updateproducto/{id}', [ProductosController::class, 'updateProducto']);
+Route::delete('/deleteproducto/{id}', [ProductosController::class, 'deleteProducto']);
 
+/* Rutas Comentarios*/
 Route::get('comentarios/{id?}', [ComentariosController::class, 'getComentarios'])->where("id", "[0-9]+");
 Route::post('comentarios', [ComentariosController::class, 'createComentario']);
+Route::put('/updatecomentario/{id}', [ComentariosController::class, 'updateComentario']);
+Route::delete('/deletecomentario/{id}', [ComentariosController::class, 'deleteComentario']);
 
+/* Rutas específicas*/
 Route::get('productos/{id}/comentarios', [ComentariosController::class, 'getComentariosPorProducto']);
 Route::get('personas/{id}/comentarios', [ComentariosController::class, 'getComentariosPorPersona']);
 
